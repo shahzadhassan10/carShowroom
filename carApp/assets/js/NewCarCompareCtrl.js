cf4fd6ae3444081ecb93ca89099275ce0b083fb3
Sajjad Khan <sajjad.khan@ebricks-inc.com>
2016-02-02 15:22:18 +0500

    validation checks before sending data to the server. code structure.

--- carApp/assets/js/NewCarCompareCtrl.js
+++ carApp/assets/js/NewCarCompareCtrl.js
@@ -4,6 +4,8 @@ NewCarCompare.controller('NewCarCompareCtrl', ['$scope', '$rootScope', 'showRoom
   var _ALL_MAKES = "All Makes";
   var _ALL_MODELS = "All Models";
   var _ALL_VERSIONS = "All Versions";
+  var valuesChanged = false;
+
   $scope.query1 = {make:_ALL_MAKES, model:_ALL_MODELS, version:_ALL_VERSIONS};
   
   $scope.allMakes1 = [_ALL_MAKES];
@@ -39,6 +41,7 @@ NewCarCompare.controller('NewCarCompareCtrl', ['$scope', '$rootScope', 'showRoom
     else{
       $scope.getAllModels1();
     }
+    valuesChanged = true;
   };
 
   $scope.allVersions1 = [_ALL_VERSIONS];
@@ -63,6 +66,7 @@ NewCarCompare.controller('NewCarCompareCtrl', ['$scope', '$rootScope', 'showRoom
     else{
       $scope.getAllVersions1();
     }
+    valuesChanged = true;
   };
 //--------------------------------------------------------------- 2
   $scope.query2 = {make:_ALL_MAKES, model:_ALL_MODELS, version:_ALL_VERSIONS};
@@ -98,6 +102,7 @@ NewCarCompare.controller('NewCarCompareCtrl', ['$scope', '$rootScope', 'showRoom
     else{
       $scope.getAllModels2();
     }
+    valuesChanged = true;
   };
 
   $scope.allVersions2 = [_ALL_VERSIONS];
@@ -122,6 +127,7 @@ NewCarCompare.controller('NewCarCompareCtrl', ['$scope', '$rootScope', 'showRoom
     else{
       $scope.getAllVersions2();
     }
+    valuesChanged = true;
   };
 //----------------------------------------------------- 3
   $scope.query3 = {make:_ALL_MAKES, model:_ALL_MODELS, version:_ALL_VERSIONS};
@@ -158,6 +164,7 @@ NewCarCompare.controller('NewCarCompareCtrl', ['$scope', '$rootScope', 'showRoom
     else{
       $scope.getAllModels3();
     }
+    valuesChanged = true;
   };
 
   $scope.allVersions3 = [_ALL_VERSIONS];
@@ -182,22 +189,85 @@ NewCarCompare.controller('NewCarCompareCtrl', ['$scope', '$rootScope', 'showRoom
     else{
       $scope.getAllVersions3();
     }
+    valuesChanged = true;
   };
   $scope.newCompCars = [];
+  $scope.tableHeadings = [];
+  $scope.showDetails = false;
+  $scope.queriedOnce = false;
 
   $scope.compareCars = function() {
+    if(!valuesChanged){
+      return;
+    }
+    valuesChanged = false;
+    
+    var isValid = $scope.isQueryValid();
+
+    if(isValid){
      showRoomService.getCarsByJson($scope.query1, $scope.query2, $scope.query3).then(function(response) {
       $scope.newCompCars = [];
-      if(response.success){ 
-        $scope.newCompCars.push({'key':'Model Year', 'val1':response.data[0].modelYear, 'val2':response.data[1].modelYear, 'val3':response.data[2].modelYear});
-        $scope.newCompCars.push({'key':'Price', 'val1':response.data[0].price, 'val2':response.data[1].price, 'val3':response.data[2].price});
-        $scope.newCompCars.push({'key':'Engine Type', 'val1':response.data[0].EngineDetails.engineType, 'val2':response.data[1].EngineDetails.engineType, 'val3':response.data[2].EngineDetails.engineType});
-        
-        console.log('cars are :' + $scope.newCompCars.length);
+      $scope.tableHeadings = [];
+      $scope.queriedOnce = true;
+
+      if(response.success && response.data != null && response.data.length >= 1){ 
+        $scope.showDetails = true;
+        var isSecPresent = (response.data.length > 1 && response.data[1] != null);
+        var isThirdPresent = (response.data.length >2 && response.data[2] != null);
+        parseHeadings(response.data, isSecPresent, isThirdPresent);
+        parseDisplayData(response.data, isSecPresent, isThirdPresent);
       } else{
-        console.log('Some error occurred.');
+        $scope.showDetails = false;
+        console.log('Some error occurred.' + response.data.errormsg);
       }
     });
+   } else {
+      $scope.showDetails = false;
+   }
+
+  };
+  var parseHeadings = function(cars, isSec, isThird){
+    var c1m = cars[0].make + ' ' + cars[0].model + ' ' + cars[0].version;
+    var c2m = isSec?cars[1].make + ' ' +  cars[1].model + ' ' + cars[1].version: '--';
+    var c3m = isThird?cars[2].make + ' ' +  cars[2].model + ' ' + cars[2].version: '--';
+    $scope.tableHeadings.push('Specs', c1m, c2m, c3m)
+  }
+  var parseDisplayData = function(cars, isSec, isThird){
+    var val2 = isSec?cars[1].modelYear:'--';
+    var val3 = isThird?cars[2].modelYear:'--';
+    $scope.newCompCars.push({'key':'Model Year', 'val1':cars[0].modelYear, 'val2':val2, 'val3':val3});
+
+    var val2 = isSec?cars[1].price:'--';    
+    val3 = isThird?cars[2].price:'--';
+    $scope.newCompCars.push({'key':'Price', 'val1':cars[0].price, 'val2':val2, 'val3':val3});
+
+    var val2 = isSec?cars[1].EngineDetails.engineType:'--';    
+    val3 = isThird?cars[2].EngineDetails.engineType:'--';
+    $scope.newCompCars.push({'key':'Engine Type', 'val1':cars[0].EngineDetails.engineType, 'val2':val2, 'val3':val3});
+  }
+
+  $scope.isQueryValid = function() {
+    var sel = 0;
+    sel += ($scope.query1.make != _ALL_MAKES)? 1 : 0;
+    sel += ($scope.query2.make != _ALL_MAKES)? 1 : 0;
+    sel += ($scope.query3.make != _ALL_MAKES)? 1 : 0;
+    if(sel >= 2){
+      sel = 0;
+      sel += ($scope.query1.model != _ALL_MODELS)? 1 : 0;
+      sel += ($scope.query2.model != _ALL_MODELS)? 1 : 0;
+      sel += ($scope.query3.model != _ALL_MODELS)? 1 : 0;
+      if(sel >= 2){
+        sel = 0;
+        sel += ($scope.query1.version != _ALL_VERSIONS)? 1 : 0;
+        sel += ($scope.query2.version != _ALL_VERSIONS)? 1 : 0;
+        sel += ($scope.query3.version != _ALL_VERSIONS)? 1 : 0;
+        return (sel >= 2);     
+      }
+    }
+    return false;
   };
 
+  $scope.showNotFound = function() {
+    return ($scope.queriedOnce && !$scope.showDetails);
+  };
  }]);
