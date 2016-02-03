a0991fe67f3dd33422d6d1d5d7a3229cdce70b32
Sajjad Khan <sajjad.khan@ebricks-inc.com>
2016-02-03 14:25:43 +0500

    added custom range filter to be used in ng-repeat. used range filter in ng-repeat for modelYear instead of hard coded years array.

--- carApp/assets/js/PostCtrl.js
+++ carApp/assets/js/PostCtrl.js
@@ -1,7 +1,7 @@
 var PostAdd = angular.module('PostAdd', []);
 
 NewCarCompare.controller('PostCtrl', ['$scope', '$rootScope', 'showRoomService', function($scope, $rootScope, showRoomService) {
-   $scope.years = [2016, 2015, 2014, 2013, 2012];
-   $scope.modelYear = $scope.years[0];
-
+   $scope.yRange = 77;
+   $scope.curYear = new Date().getFullYear();
+   $scope.modelYear = "";
 }]);
